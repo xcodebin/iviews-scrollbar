622cadaf360a851f4d49cc99e8382216d3796116
xcodebin <xb865453422@hotmail.com>
2017-06-19 21:33:15 +0800

    demo

---
+++ examples/components/test.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>{{ row.name }}</div>
+</template>
+<script>
+    export default {
+        props: {
+            row: Object
+        },
+        mounted () {
+            console.log(1);
+        },
+        beforeDestroy () {
+            console.log(2);
+        }
+    }
+</script>
